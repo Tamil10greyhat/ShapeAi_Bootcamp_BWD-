4ad285e66d60b56cea1cd0eac9afad9bd7ef2c74
Tamil Amudhan <85351257+Tamil10greyhat@users.noreply.github.com>
2021-06-28 10:50:34 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript And React.js</h1>
+      <p>A basic Javascript and react js</p>
+    </div>
+  );
+}
+
+export default Info;
